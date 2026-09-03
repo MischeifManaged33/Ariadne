0e2e8e9ef282642fad0b35b3004d269a63084d63
e.albright33 <e.albright33@gmail.com>
2026-09-03 18:32:42 -0500

    added simple random walk algo

---
+++ Assets/Scripts/ProceduralGenerationAlgorithms.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using Systems.Collections.Generic;
+
+public static class ProceduralGenerationAlgorithms
+{
+    public static HashSet<Vector2Int> SimpleRandomWalk(Vector2Int startPos, int walkLength)
+    {
+        HashSet<Vector2Int> path = new HashSet<Vector2Int>();
+        var prevPos = startPos;
+
+        for (int i = 0; i < walkLength; i++)
+        {
+            var newPos = prevPos + Direction2D.GetRandCardDir();
+            path.Add(newPos);
+            prevPos = newPos;
+        }
+        return path;
+    }
+}
+
+public static class Direction2D
+{
+    public static List<Vecotr2Int> cardinalDirList = new List<Vector2Int>
+    {
+        new Vector2Int(0, 1), //UP
+        new Vector2Int(1, 0), //RIGHT
+        new Vector2Int(0, -1), //DOWN
+        new Vector2Int(-1, 0) //LEFT
+    };
+
+    public static Vector2Int GetRandCardDir()
+    {
+        return cardinalDirList[Random.Range(0, cardinalDirList.count)];
+    }
+}
