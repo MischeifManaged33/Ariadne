ad8305137d100c4cdf75ec4cf6b9ef3cf9eb291d
e.albright33 <e.albright33@gmail.com>
2026-09-22 11:03:06 -0500

    fixed collisions

--- Assets/_Sprites/Tiled/TileMapGrid_png_test3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="TileMapGrid_png_test3" tilewidth="280" tileheight="280" tilecount="225" columns="15">
- <image source="../../TileMapGrid_png_test3.png" width="4200" height="4200"/>
-</tileset>
